c71cb5ba19ee2aec2c8af0d1bfa90dd7c9b278c2
Kareem Ebrahem <safen894@gmail.com>
2023-09-29 06:52:14 +0300

    gta4l: Change local_path dir from gta4lwifi to gta4l

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/samsung/gta4lwifi
+LOCAL_PATH := device/samsung/gta4l
 
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
