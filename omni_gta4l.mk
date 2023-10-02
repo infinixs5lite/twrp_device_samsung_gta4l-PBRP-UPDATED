3ef6419df396619bf116961a772a55149613c1a0
infinixs5lite <kelvinchinedu97@gmail.com>
2023-10-02 06:57:46 +0100

    Update omni_gta4l.mk

--- omni_gta4l.mk
+++ omni_gta4l.mk
@@ -18,6 +18,10 @@
 
 PRODUCT_RELEASE_NAME := gta4l
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from twrp product configuration
 $(call inherit-product, vendor/pb/config/common.mk)
 
